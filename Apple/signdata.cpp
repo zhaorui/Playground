5888fcb2ef9fc9bb8f15fbbd2f825ba84125fabf
zhaorui <zhaoruiexplorer@gmail.com>
2014-08-11 17:59:45 +0800

    Finish the Sample for using CSSM_SignData, this API seems got bug on 10.10

--- Apple/signdata.cpp
+++ Apple/signdata.cpp
@@ -2,9 +2,14 @@
 #include <Security/cssmapi.h>
 #include <Security/SecBase.h> //SecKeychainRef
 #include <Security/SecKeychain.h> //SecKeychainCopySearchList
+//#include <Security/SecIdentity.h> //SecIdentityCopyCertificate ????
+#include <Security/SecIdentity.h>
 #include <Security/SecIdentitySearch.h> 
+#include <Security/SecKey.h> // SecKeyGetCSSMKey
 #include <CoreFoundation/CoreFoundation.h>
+#include <openssl/sha.h> //SHA1
 #include <stdlib.h>
+#include <iostream>
 using namespace std;
 
 //copy from TRUNK/include/darwin/secwrap.h
@@ -70,9 +75,21 @@ struct CssmData : public CSSM_DATA
 
 int main(int argc, char ** argv)
 {
-    CssmContext cssmContext;
+    CSSM_DATA data;
     CSSM_DATA in, out;
     OSStatus ret;
+    CSSM_RETURN crtn;
+
+    // Hardcode the data
+    unsigned char buffer[] = "Hello,World";
+    data.Data = buffer;
+    data.Length = 11;
+
+    //Initiazlize the in
+    uint8 sha1Buf[SHA_DIGEST_LENGTH];
+    SHA1(data.Data, data.Length, sha1Buf);
+    in.Data = sha1Buf;
+    in.Length = sizeof(sha1Buf);
 
     //Get the KeychianRef first
     SecKeychainRef keychainRef;
@@ -80,37 +97,99 @@ int main(int argc, char ** argv)
     SecKeychainCopySearchList(&keychainList);
     CFRetain(keychainList);
     keychainRef = (SecKeychainRef) CFArrayGetValueAtIndex(keychainList, 0);
-    
-    //Get the private key
-    SecIdentitySearchRef idSearch = 0;
-    SecIdentityRef idRef;
-    SecKeyRef privateKeyRef;
-    SecCertificateRef certRef;
 
-    ret = SecIdentitySearchCreate(0, CSSM_KEYUSE_SIGN, &idSearch);
-    while (true){
-        //find the specific private key?
-        ret = SecIdentitySearchCopyNext(idSearch, &idRef);
-        ret = SecIdentityCopyCertificate(idRef, &certRef);
-        ret = SecIdentityCopyPrivateKey(idRef, &privateKeyRef);
-    }
-
-
-
-    //Get CSP Handle
+    //Get the CSP (Cryptographic Sevice Provider) Handle
     CSSM_CSP_HANDLE csp;
     ret = SecKeychainGetCSPHandle(keychainRef, &csp);
     if (ret)
     {
-        cerr<<"SecKeychainGetCSPHandle failed"<<endl;
+        cout<<"SecKeychainGetCSPHandle failed"<<endl;
         exit(-1);
     }
 
-    //
+    //Create IdentitySearchReference
+    SecIdentitySearchRef idSearch = 0;
+    ret = SecIdentitySearchCreate(0, CSSM_KEYUSE_SIGN, &idSearch);
+    if (ret)
+    {
+        cout<<"SecIdentitySearchCreate failed"<<endl;
+        exit(-1);
+    }
 
+    //traverse the identities, and get cert and privateKey reference
+    SecIdentityRef idRef;
+    //CSSM_DATA cert;
+    SecKeyRef privateKeyRef;
+    while (true)
+    {
+        ret = SecIdentitySearchCopyNext(idSearch, &idRef);
+        if (ret)
+        {
+            cout<<"could not find identity"<<endl;
+        }
+        //ret = SecIdentityCopyCertificate(idRef, &cert);
+        //ret = SecIdentityCopyCertificate(idRef, &cert);
+        //if (ret)
+        //{
+        //    cout<<"Could not copy certificate"<<endl;
+        //}
+        ret = SecIdentityCopyPrivateKey(idRef, &privateKeyRef);
+        if (ret == noErr)
+        {
+            break;
+        }
+    }
 
-    //CSSM_CSP_CreateSignatureContext();
-    CSSM_SignData(cssmContext, &in, 1, CSSM_ALGID_NONE, &out);
+    //Get the private key
+    const CSSM_KEY *key;
+    ret = SecKeyGetCSSMKey((SecKeyRef) privateKeyRef, &key);
+    if (ret)
+    {
+        cout<<"SecKeyGetCSSMKey failed"<<endl;
+        exit(-1);
+    }
+
+    //undocumented API that lets us use the key, where to use ???
+    const CSSM_ACCESS_CREDENTIALS* creds;
+    ret = SecKeyGetCredentials(privateKeyRef,
+          CSSM_ACL_AUTHORIZATION_SIGN, kSecCredentialTypeDefault,
+          &creds);
+    if (ret)
+    {
+        cout<<"SecKeyGetCredentials failed"<<endl;
+        exit(-1);
+    }
+
+    //create signature context
+    CssmContext cssmContext;
+    crtn = CSSM_CSP_CreateSignatureContext(csp, CSSM_ALGID_RSA, creds, key, &cssmContext);
+    if (crtn)
+    {
+        cout<<"CSSM_CSP_CreateSignatureContext failed"<<endl;
+        exit(-1);
+    }
+
+    //update padding attirbute
+    CSSM_CONTEXT_ATTRIBUTE paddingAttribute;
+    CSSM_PADDING padding = CSSM_PADDING_PKCS1;
+    paddingAttribute.AttributeType = CSSM_ATTRIBUTE_PADDING;
+    paddingAttribute.AttributeLength = sizeof(padding);
+    paddingAttribute.Attribute.Data = (CSSM_DATA_PTR) padding;
+    crtn = CSSM_UpdateContextAttributes(cssmContext, 1, &paddingAttribute);
+
+    //sign the data
+    crtn = CSSM_SignData(cssmContext, &in, 1, CSSM_ALGID_NONE, &out);
+    if (crtn)
+    {
+        cout<<"CSSM_SignData failed"<<endl;
+        cout<<"ErrCode: "<<hex<<crtn<<" "<<dec<<crtn<<endl;
+        cssmPerror("SignData Error Message", crtn);
+    }
+    else
+    {
+        cout<<"Length: "<<out.Length<<endl;
+        cout<<"Data: "<<out.Data<<endl;
+    }
 
 
     CFRelease(keychainList);
